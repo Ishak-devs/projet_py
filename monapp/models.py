2f7662c32451e8756b7639b485490c34e16f486d
ishak <i.kouici@insta.fr>
2025-08-12 22:17:23 +0200

    add first page

--- monapp/models.py
+++ monapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Calendrier(models.Model):
+    jour = models.CharField(max_length=100)
+    heure_debut = models.TimeField()
+    heure_fin = models.TimeField()
+
+    def __str__(self):
+        return f"{self.jour} : {self.heure_debut} - {self.heure_fin}"
