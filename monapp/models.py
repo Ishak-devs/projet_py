bf9bbdaecb0edb9e7821598254dbb2d9be1825d9
ishak <i.kouici@insta.fr>
2025-08-13 11:03:05 +0200

    ajout du form

--- monapp/models.py
+++ monapp/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Calendrier(models.Model):
+class Evenements(models.Model):
+    Titre = models.CharField(max_length=100)
     jour = models.CharField(max_length=100)
     heure_debut = models.TimeField()
     heure_fin = models.TimeField()
+    infos_complementaires = models.CharField(max_length=500)
 
     def __str__(self):
         return f"{self.jour} : {self.heure_debut} - {self.heure_fin}"
--- monapp/views.py
+++ monapp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def accueil(request):
     return render(request, "index.html")
+
+def Ajout_event():
+
