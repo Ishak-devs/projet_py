d0a27ad8ce70ce3fe8f16fdcb5d55f6c075aa8f7
ishak <i.kouici@insta.fr>
2025-08-13 11:09:34 +0200

    ajout du form

--- monapp/views.py
+++ monapp/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from models import Evenements
 
 # Create your views here.
 def accueil(request):
     return render(request, "index.html")
 
-def Ajout_event():
-
+def Ajout_event(request):
+    if request.method == 'POST':
+            Titre = request.POST.get('Titre')
+            oroganisateur_nom = request.POST.get('organisateur_nom')
