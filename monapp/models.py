8c3098fc6f158302ba4899155e43fda20aebb6ce
ishak <i.kouici@insta.fr>
2025-09-01 16:00:23 +0200

    commit

--- monapp/models.py
+++ monapp/models.py
@@ -6,5 +6,7 @@ class Evenements(models.Model):
     Titre = models.CharField(max_length=100)
     organisateur_nom = models.CharField(max_length=150 , default="inconnu")
 
+
     def __str__(self):
         return f"{self.Titre} : {self.organisateur_nom}"
+
