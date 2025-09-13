545b5c02d0e038f40095724cbffe757320d2832f
kondorxet <60437553+kondorxet@users.noreply.github.com>
2025-09-13 15:34:15 +0200

    views changé et ajout champs dans model evenements
    models.py changé
    views.py changé
    dependances peut etre changés jai oublié

--- monapp/models.py
+++ monapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Evenements(models.Model):
     Titre = models.CharField(max_length=100)
-    organisateur_nom = models.CharField(max_length=150 , default="inconnu")
+    organisateur_nom = models.CharField(max_length=150 , default="Inconnu")
+    lieu = models.CharField(max_length=150 , default="En ligne")
+    date = models.DateField(default=datetime.now)
+
 
 
     def __str__(self):
