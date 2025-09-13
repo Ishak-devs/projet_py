ffb4eeee5f9c5965bc3da8032154441dc35e551f
ishak <i.kouici@insta.fr>
2025-09-13 15:49:41 +0200

    okay for display event

--- monapp/models.py
+++ monapp/models.py
@@ -6,7 +6,6 @@ class Evenements(models.Model):
     Titre = models.CharField(max_length=100)
     organisateur_nom = models.CharField(max_length=150 , default="inconnu")
 
-
     def __str__(self):
         return f"{self.Titre} : {self.organisateur_nom}"
 
